dadfa5c96307b13b1944217c329473482ee926b6
afifa sultana <afifa_sultana@yahoo.com>
2021-02-23 21:36:00 +0100

    my project

---
+++ home.js
@@ -0,0 +1,20 @@
+
+console.log('hello');
+			console.log('hello');
+			
+
+			var b= 'Anas';
+			var a ='Ayan';
+
+			console.log(b);
+			console.log(a);
+
+			var number= 45;
+			console.log(number);
+
+			//var age =prompt ('what is your age?');
+
+   
+            document.getElementById('sometext').innerHTML = 'Hello world';
+			
+
---
+++ main.js
@@ -0,0 +1,22 @@
+//var number1=35;
+//var number2=45;
+//alert(number1+number2);
+
+//var number1='35';
+//var number2='45';
+//alert("My favourite number is "+number2);
+
+//camel case;
+//
+//var myFavouriteNumber;
+//Prtial case;
+
+//var MyFvouriteNumber;
+//Array
+
+//var colors=['red','blue','green'];
+//colors.push('purple');
+//alert (colors[3]);
+
+var numbers=[1,2,3,'Seven'];
+alert(numbers[3]);
